be700f830d026b8cc4b26879f6980db27f24a0a2
Sotiris Niarchos <sot.niarchos@gmail.com>
2019-09-11 11:57:22 +0300

    modified all.sh test script

--- all.sh
+++ all.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-for test in $@; do
-	./alanc $test -a -O
+# checks only for compilation errors (overwrites executables)
+# agnostic as to whether the executable, if produced, works as expected
+
+not_working=()
+
+for infile in $(find test/ | grep ".alan"); do
+	echo "compiling $infile"
+	./alanc $infile
+	if [[ $? -ne $zero ]]; then
+		not_working+=($infile)
+	fi
+done
+
+# report programs that did not compile
+echo ""
+echo "programs that did not compile:"
+for i in "${not_working[@]}"; do
+   echo "$i"
 done
